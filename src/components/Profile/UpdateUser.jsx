3ef9dd64cca2cfbadf9f74ae59d3573098f8ca9a
FernandoRR73 <cunete.f@gmail.com>
2024-05-27 19:59:48 +0200

    update user

--- src/components/Profile/UpdateUser.jsx
+++ src/components/Profile/UpdateUser.jsx
@@ -1,93 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
+import { AuthContext } from '../../AuthContext';
 import { Form, Button, Alert, Container, Row, Col } from 'react-bootstrap';
 
-const UpdateUser = ({ onUpdate }) => {
+const UpdateUser = () => {
+  const { user, setUser } = useContext(AuthContext);
   const [newEmail, setNewEmail] = useState('');
   const [newUsername, setNewUsername] = useState('');
   const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState('');
   const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    
-    if (newPassword && newPassword !== confirmPassword) {
-      setError('Las contraseñas no coinciden');
-      return;
-    }
-
-    setError(''); // Clear any previous errors
-
     try {
-      const response = await fetch('http://localhost:3001/auth/updateUser', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        credentials: 'include', // Asegúrate de incluir las cookies de sesión
-        body: JSON.stringify({ newEmail, newUsername, newPassword })
-      });
+      await axios.put('http://localhost:3001/auth/updateUser', {
+        newEmail,
+        newUsername,
+        newPassword
+      }, { withCredentials: true });
 
-      if (response.ok) {
-        const data = await response.json();
-        onUpdate(newUsername);
-        setSuccess('Perfil actualizado exitosamente');
-        setError('');
-      } else {
-        const data = await response.json();
-        setError(data.message || 'Error al actualizar usuario');
-        setSuccess('');
-      }
+      setMessage('Perfil actualizado exitosamente');
+      setUser({ ...user, email: newEmail || user.email, username: newUsername || user.username });
     } catch (error) {
-      setError('Error al actualizar usuario');
-      setSuccess('');
-      console.error('Error al actualizar usuario:', error);
+      setError('Error al actualizar el perfil');
+      console.error('Error al actualizar el perfil:', error);
     }
   };
 
   return (
-    <Container className="d-flex justify-content-center align-items-center form-container">
+    <Container className="d-flex justify-content-center pt-5">
       <Row className="w-100">
         <Col xs={12} md={6} lg={4} className="mx-auto">
           <Form onSubmit={handleSubmit}>
-            <h3>Actualizar Usuario</h3>
+            <h2>Actualizar Perfil</h2>
+            {message && <Alert variant="success">{message}</Alert>}
             {error && <Alert variant="danger">{error}</Alert>}
-            {success && <Alert variant="success">{success}</Alert>}
-            <Form.Group controlId="formNewEmail">
-              <Form.Label>Nuevo Correo Electrónico:</Form.Label>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Correo Electrónico:</Form.Label>
               <Form.Control
                 type="email"
+                placeholder={user.email}
                 value={newEmail}
                 onChange={(event) => setNewEmail(event.target.value)}
-                className="custom-input"
               />
             </Form.Group>
-            <Form.Group controlId="formNewUsername">
-              <Form.Label>Nuevo Usuario:</Form.Label>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Nombre de Usuario:</Form.Label>
               <Form.Control
                 type="text"
+                placeholder={user.username}
                 value={newUsername}
                 onChange={(event) => setNewUsername(event.target.value)}
-                className="custom-input"
               />
             </Form.Group>
-            <Form.Group controlId="formNewPassword">
-              <Form.Label>Nueva Contraseña (opcional):</Form.Label>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Nueva Contraseña:</Form.Label>
               <Form.Control
                 type="password"
                 value={newPassword}
                 onChange={(event) => setNewPassword(event.target.value)}
-                className="custom-input"
-              />
-            </Form.Group>
-            <Form.Group controlId="formConfirmPassword">
-              <Form.Label>Confirmar Nueva Contraseña:</Form.Label>
-              <Form.Control
-                type="password"
-                value={confirmPassword}
-                onChange={(event) => setConfirmPassword(event.target.value)}
-                className="custom-input"
               />
             </Form.Group>
             <Button variant="primary" type="submit" className="custom-button">
